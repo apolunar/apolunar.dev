ec9ae623efe22f8e6518aafc670714367c1960fe
apolunar <isaac.holmdahl@protonmail.com>
2024-02-08 01:03:04 -0600

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import sitemap from '@astrojs/sitemap';
 import icon from "astro-icon";
 import robotsTxt from "astro-robots-txt";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://apolunar.dev',
-  integrations: [mdx(), sitemap(), icon(), robotsTxt()]
+  integrations: [mdx(), sitemap(), icon(), robotsTxt()],
+  output: "server",
+  adapter: cloudflare({ mode: "directory" })
 });
